f51944876dc14da59606160ff1dc8a119fe3841a
tb832f <tb832f@MOCDTL77JO5020.ITServices.sbc.com>
2014-10-31 09:54:20 -0500

    added the mygitsandboxhelper class

---
+++ myGitSandbox/src/myGitSandbox/MyGitSandboxHelper.java
@@ -0,0 +1,15 @@
+package myGitSandbox;
+
+public class MyGitSandboxHelper 
+{
+	MyGitSandboxHelper()
+	{
+		//Do some initializing business in here...
+	}
+	
+	public void getHelp()
+	{
+		//get some help here....
+	}
+
+}
