935248fa3ce6250185ca2c9c701a787d959d9380
tb832f <tb832f@MOCDTL77JO5020.ITServices.sbc.com>
2014-11-04 15:41:05 -0600

    added in max and min stuff

--- myGitSandbox/src/myGitSandbox/MyGitSandbox.java
+++ myGitSandbox/src/myGitSandbox/MyGitSandbox.java
@@ -2,9 +2,22 @@ package myGitSandbox;
 
 public class MyGitSandbox 
 {
+	static Integer MAX = Integer.MAX_VALUE;
+	static Integer MIN = Integer.MIN_VALUE;
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, OLE team!");
+		printMax();
+		printMin();
+	}
+	public static void printMax()
+	{
+		System.out.println(MAX);
+	}
+	public static void printMin()
+	{
+		System.out.println(MIN);
+		
 	}
 
 }
