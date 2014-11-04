60816af85f6790abd41386f47d45904480e7bbdd
tbarker23 <barker.tommy@gmail.com>
2014-11-04 15:37:34 -0600

    added the printstring method

--- myGitSandbox/src/myGitSandbox/MyGitSandbox.java
+++ myGitSandbox/src/myGitSandbox/MyGitSandbox.java
@@ -2,9 +2,15 @@ package myGitSandbox;
 
 public class MyGitSandbox 
 {
+	static String stuff = "stuff";
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, OLE team!");
+		printString(stuff);
+	}
+	public static void printString(String s)
+	{
+		System.out.println(s);
 	}
 
 }
